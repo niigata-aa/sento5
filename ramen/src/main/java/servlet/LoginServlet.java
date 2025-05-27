fe7a5f1d764c21e8fb16d433dd76c045f08fd097
emtech-user <emtech-user@PC-number>
2025-05-27 16:02:48 +0900

    Merge branch 'master' of https://github.com/niigata-aa/sento5.git ログイン、ログアウトサーブレット

--- ramen/src/main/java/servlet/LoginServlet.java
+++ ramen/src/main/java/servlet/LoginServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.DAO.UserDAO;
 
 /**
  * Servlet implementation class LoginServlet
@@ -34,8 +40,39 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		//リクエストパラメーターの取得
+		int userId = Integer.parseInt(request.getParameter("userId"));
+		String password =request.getParameter("password");
+		String url = null;
+
+		/**
+		 * ログイン処理
+		 * パスワードが一致したらuser_idをセッションに設定
+		 */
+		UserDAO dao = new UserDAO();
+		try {
+			if(dao.loginCheck(userId, password)) {
+				HttpSession session = request.getSession();
+				session.setAttribute("userId", userId);
+				int roll=dao.rollCheck(userId);
+				session.setAttribute("roll",roll);
+				url="top.jsp";
+			}else {
+				url="login.html";
+				request.setAttribute("errorMessage", "ユーザー名またはパスワードが正しくありません。");
+				//エラーメッセージを表示させたい		
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		//リクエストの転送
+		RequestDispatcher rd = request.getRequestDispatcher(url);
+		rd.forward(request, response);
 	}
 
 }
--- ramen/src/main/webapp/login.jsp
+++ ramen/src/main/webapp/login.jsp
@@ -1,3 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +9,9 @@
 <body>
 <h2>ログインページ</h2>
 <h2>ユーザ登録</h2>
+<%if(request.getAttribute("errorMessage")!=null){%>
+<%=request.getAttribute("errorMessage")%>
+<%} %>
 <form action="login-servlet"method="POST">
 ユーザID：<input type="text" name="userName"value="" placeholder="ユーザID"><br>
 パスワード：<input type="password" name=""value="" placeholder="パスワード"><br>
