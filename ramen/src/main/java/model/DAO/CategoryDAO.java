902eb0b85d5e076a2657326592de147561601931
emtech-user <emtech-user@PC-number>
2025-05-27 09:30:39 +0900

    初回コミット加藤

---
+++ ramen/src/main/java/model/DAO/CategoryDAO.java
@@ -0,0 +1,51 @@
+package model.DAO;
+
+public class CategoryDAO {
+	
+	package model.dao;
+
+	import java.sql.Connection;
+	import java.sql.PreparedStatement;
+	import java.sql.ResultSet;
+	import java.sql.SQLException;
+	import java.util.ArrayList;
+	import java.util.List;
+
+	/*
+	 *m_genreテーブルのDAO 
+	 * @return genrelist
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+
+	public class CategoryDAO {
+
+		public List<GenreBean> selectCategory() throws SQLException, ClassNotFoundException{
+			
+			List<GenreBean> genrelist = new ArrayList<GenreBean>();
+			
+			String sql = "select genre_name from m_genre";
+			
+			try(Connection con = ConnectionManager.getConnection();
+					PreparedStatement pstmt = con.prepareStatement(sql);
+					ResultSet res = pstmt.executeQuery()){
+				
+				while (res.next()) {
+					String GenreName = res.getString("GenreName");
+					
+					GenreBean genre = new GenreBean();
+					genre.setGenreName(GenreName);
+				}
+			}catch(SQLException e) {
+				System.out.print(e);
+				e.printStackTrace();
+				throw e;
+			}
+			return genrelist;
+			
+		}
+		
+	}
+
+
+}
