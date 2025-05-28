7fa87dc536180b39c5f2b38fe3b58633c2f93eb6
emtech-user <emtech-user@PC-number>
2025-05-28 09:38:40 +0900

    削除、削除確認サーブレット

--- ramen/src/main/java/servlet/UserDeleteConfirmServlet.java
+++ ramen/src/main/java/servlet/UserDeleteConfirmServlet.java
@@ -1,11 +1,14 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class UserDeleteConfirmServlet
@@ -34,8 +37,19 @@ public class UserDeleteConfirmServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		//リクエストパラメーターの取得
+		String d_userId = request.getParameter("userId");
+		String d_userName = request.getParameter("userName");
+
+		//リクエストスコープの設定
+		HttpSession session = request.getSession();
+		session.setAttribute("d_userId", d_userId);
+		session.setAttribute("d_userName", d_userName);
+
+		//リクエストの転送
+		RequestDispatcher rd = request.getRequestDispatcher("userDeleteConfirm.jsp");
+		rd.forward(request, response);
 	}
 
 }
--- ramen/src/main/java/servlet/UserDeleteServlet.java
+++ ramen/src/main/java/servlet/UserDeleteServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.DAO.UserDAO;
 
 /**
  * Servlet implementation class UserDeleteServlet
@@ -34,8 +39,25 @@ public class UserDeleteServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		
+		HttpSession session = request.getSession();
+		int userId = (int) session.getAttribute("d_useId");
+		
+		UserDAO dao =new UserDAO();
+		try {
+			dao.deleteUser(userId);
+		} catch (ClassNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		session.removeAttribute("d_userId");
+		session.removeAttribute("d_userName");
+		
+				
 	}
 
 }
