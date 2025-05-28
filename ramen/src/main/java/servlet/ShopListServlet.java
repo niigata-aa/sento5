402c9422c8b85877357325d43282960fc23077ae
emtech-user <emtech-user@PC-number>
2025-05-28 10:58:19 +0900

    ショップリストサーブの作成　齊藤

--- ramen/src/main/java/servlet/ShopListServlet.java
+++ ramen/src/main/java/servlet/ShopListServlet.java
@@ -1,11 +1,18 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Bean.ShopBean;
+import model.DAO.ShopDAO;
 
 /**
  * Servlet implementation class ShopListServlet
@@ -35,7 +42,32 @@ public class ShopListServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String url = null;
+		//セッションオブジェクトの取得
+		HttpSession session = request.getSession();
+		
+		//ログイン確認済みかどうかを確認
+		if(session.getAttribute("userId") != null) {
+			//認証済み
+			try {
+				ShopDAO shopDao = new ShopDAO();
+				List<ShopBean> shopList = shopDao.selectAllshop();
+				request.setAttribute("shopList", shopList);
+				url = "shopList.jsp";
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			RequestDispatcher rd = request.getRequestDispatcher(url);
+			rd.forward(request,response);
+			
+	}	else {
+		//未認証
+		url = "Login.jsp";
+	}
 	}
 
 }
