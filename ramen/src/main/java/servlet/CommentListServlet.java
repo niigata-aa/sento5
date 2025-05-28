e521dc997ba2a2244ccd44dce0430b4d84b5fec0
emtech-user <emtech-user@PC-number>
2025-05-28 11:45:55 +0900

    更新

--- ramen/src/main/java/servlet/CommentListServlet.java
+++ ramen/src/main/java/servlet/CommentListServlet.java
@@ -1,11 +1,18 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Bean.CommentBean;
+import model.DAO.CommentDAO;
 
 /**
  * Servlet implementation class ComentListServlet
@@ -35,7 +42,25 @@ public class CommentListServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		
+		int userId=Integer.parseInt(request.getParameter("userId"));
+		
+		List<CommentBean> commentList=null;
+		
+		CommentDAO dao=new CommentDAO();
+		
+		try {
+			commentList=dao.selectUserComment(userId);
+		}catch (SQLException  |  ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		request.setAttribute("commentList", commentList);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("comment.jsp");
+		
+		rd.forward(request, response);
+		
 	}
 
 }
